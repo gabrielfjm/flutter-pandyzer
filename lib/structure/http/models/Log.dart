faa9dd1de8c80b040a9c7dbb951f27c2043973c2
Gabriel Felipe Jess Meira <bibodev04@gmail.com>
2025-10-26 19:10:54 -0300

    FEATURE#10-retirado o relatório individual e ajuste no log

--- lib/structure/http/models/Log.dart
+++ lib/structure/http/models/Log.dart
@@ -1,33 +1,33 @@
-import 'User.dart';
+  import 'User.dart';
 
-class Log {
-  final int id;
-  final String description;
-  final User user;
-  final String logTimestamp;
+  class Log {
+    final int id;
+    final String description;
+    final User user;
+    final String logTimestamp;
 
-  Log({
-    required this.id,
-    required this.description,
-    required this.user,
-    required this.logTimestamp,
-  });
+    Log({
+      required this.id,
+      required this.description,
+      required this.user,
+      required this.logTimestamp,
+    });
 
-  factory Log.fromJson(Map<String, dynamic> json) {
-    return Log(
-      id: json['id'],
-      description: json['description'],
-      user: User.fromJson(json['user']),
-      logTimestamp: json['logTimestamp'],
-    );
+    factory Log.fromJson(Map<String, dynamic> json) {
+      return Log(
+        id: json['id'],
+        description: json['description'],
+        user: User.fromJson(json['user']),
+        logTimestamp: json['logTimestamp'],
+      );
+    }
+
+    Map<String, dynamic> toJson() {
+      return {
+        'id': id,
+        'description': description,
+        'user': user.toJson(),
+        'logTimestamp': logTimestamp,
+      };
+    }
   }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'description': description,
-      'user': user.toJson(),
-      'logTimestamp': logTimestamp,
-    };
-  }
-}
